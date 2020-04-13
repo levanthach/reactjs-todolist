7ea36e1f655c8ee8af0e45a927aa314059b5a5de
Lê Văn Thạch <lethach@vinacis.com>
2020-04-13 22:59:29 +0700

    up

---
+++ precache-manifest.8ed45b0ca353656f9131557c47232ed9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f056319385aeeb30a17f85d82ddea54f",
+    "url": "/reactjs-todolist/index.html"
+  },
+  {
+    "revision": "49ffc81bd410f69c0c05",
+    "url": "/reactjs-todolist/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fa05be36b087f72cd0e0",
+    "url": "/reactjs-todolist/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "49ffc81bd410f69c0c05",
+    "url": "/reactjs-todolist/static/js/2.d7436e3e.chunk.js"
+  },
+  {
+    "revision": "7167253e62cc05ddfb6a351681d034ad",
+    "url": "/reactjs-todolist/static/js/2.d7436e3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa05be36b087f72cd0e0",
+    "url": "/reactjs-todolist/static/js/main.c5012f23.chunk.js"
+  },
+  {
+    "revision": "b37a3419379cca4e4f5e",
+    "url": "/reactjs-todolist/static/js/runtime-main.1276340e.js"
+  }
+]);
